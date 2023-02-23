9fd71904b83bd102a87f15a3d17e26fa7bc79ed1
MariaLanss <maruska0990@gmail.com>
2023-02-23 12:38:27 +0300

    Домашка

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,13 @@ public class Main {
 
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
-        int[] arr = new int [30];
+        int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
         }
         return arr;
     }
+
     public static void task1() {
         int[] arr = generateRandomArray();
         int sum = 0;
@@ -53,19 +54,17 @@ public class Main {
     }
 
     public static void task4() {
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        reverseFullName[0] = 'I';
-        reverseFullName[1] = 'v';
-        reverseFullName[2] = 'a';
-        reverseFullName[3] = 'n';
-        reverseFullName[4] = 'o';
-        reverseFullName[5] = 'v';
-        reverseFullName[6] = ' ';
-        reverseFullName[7] = 'I';
-        reverseFullName[8] = 'v';
-        reverseFullName[9] = 'a';
-        reverseFullName[10] = 'n';
-        System.out.println(reverseFullName);
-    }
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
 
+        int n = reverseFullName.length;
+        char temp;
+        for (int i = 0; i < n/2; i++) {
+            temp = reverseFullName[n - i - 1];
+            reverseFullName[n - i - 1] = reverseFullName[i];
+            reverseFullName[i] = temp;
+        }
+        for (int i = 0; i < reverseFullName.length; i++) {
+            System.out.print(reverseFullName[i]);
+        }
+    }
 }
